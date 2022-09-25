720f0126827e31fba3afc2644945a797d6efaf37
Gabriel Ribeiro <ribeiroggabriel@gmail.com>
2022-09-25 13:56:35 -0300

    Folder and project setup: - Setup.py file as installable with -e - Gitignore file added - Static, Templates and Test folders for development

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="flask-app-tdd",
+    version="0.0.1",
+    description="A simple Named Entity Recognition API",
+    packages=find_packages(),
+)
